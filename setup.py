801671e068ab6467e6e1adcc8b13bb9aee98f827
tpchuckles <tpchuckles@gmail.com>
2024-02-01 16:39:32 -0500

    setup.py file created

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TDTR_fitting',
+    version='0.161',    
+    description='fitting code for thermoreflectance experiments',
+    url='https://github.com/ExSiTE-Lab/TDTR_fitting',
+    author='Thomas W. Pfeifer',
+    author_email='twp4fg@virginia.edu',
+    packages=['TDTR_fitting'],
+    install_requires=['numpy','sys'],
+)
