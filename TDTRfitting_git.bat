c31514ec0c3356f3e15675f74ebd157769052003
tpchuckles <tpchuckles@gmail.com>
2025-01-16 09:47:02 -0500

    Create TDTRfitting_git.bat

---
+++ TDTRfitting_git.bat
@@ -0,0 +1,5 @@
+curl https://raw.githubusercontent.com/ExSiTE-Lab/TDTR_fitting/refs/heads/main/TDTR_fitting.py -o TDTR_fitting.py
+curl https://raw.githubusercontent.com/ExSiTE-Lab/TDTR_fitting/refs/heads/main/gui.py -o gui.py
+curl https://raw.githubusercontent.com/ExSiTE-Lab/niceplot/refs/heads/main/niceplot.py -o niceplot.py
+curl https://raw.githubusercontent.com/ExSiTE-Lab/niceplot/refs/heads/main/nicecontour.py -o nicecontour.py
+py gui.py
